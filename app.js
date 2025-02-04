e544a45163ec6792683dc93e5d78c9806f9be9d5
NetDev24 <ernestofm2001@gmail.com>
2025-02-04 15:02:42 -0600

    Creacion de funcion agregaAmigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables globales
+let personas = [];
+
+function agregarAmigo(){
+    let personaIntroducida = document.getElementById("amigo").value;
+    if(personaIntroducida == "" || personaIntroducida == null){
+        alert("Ingrese un nombre valido");
+    }else{
+        personas.push(personaIntroducida);
+        console.log(personaIntroducida);
+        actualizarLista();
+    } 
+    
+}
+
+function actualizarLista(){
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML="";
+    personas.forEach(persona => {
+        let li = document.createElement("li");
+        li.textContent = persona;
+        ul.appendChild(li);
+    });
+
+}
+
+
